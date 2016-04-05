cfcdd0ff0a968b12e8fca1e40988918a324a6f6a
Denis Nelubin <dnelubin@gmail.com>
2016-04-05 17:19:50 +0600

    Added small Maven project for JavaDoc generation.

---
+++ src/main/java/Test.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * This is just a test class.
+ * Want to check how JavaDoc for it is generated.
+ */
+
+public class Test {
+
+    /**
+     *  Main entry point.
+     *  @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+
+}
